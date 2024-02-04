ba3b4b778790c18d5847285e619953c2855ce2ad
alikhosravi <akhosravi71@yahoo.com>
2024-02-04 22:29:21 +0330

    update

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from geometry2tiles import TileList
+from geometry2tiles import TileList, TileFrame
 from downloaders    import cartodb_light, cartodb_dark, osm, lima, satellite, bing_satellite, bing_road
 
 zoom = 8
-tiles = TileList.LOT('data/irn_admbnda_adm0_unhcr_20190514.shp',zoom)
 
+tiles = TileFrame.Frame('data/irn_admbnda_adm0_unhcr_20190514.shp',zoom)
+'''
+tiles = TileList.LOT('data/irn_admbnda_adm0_unhcr_20190514.shp',zoom)
 for tile in tiles:
     x, y = tile
     reference = bing_satellite #change reference
     reference.downloader(zoom, x, y)
 
+'''
