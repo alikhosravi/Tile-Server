5de24a6c5f8c71119d5eed4f44ee67e5485745e8
Ali Khosravi Kazazi <akhosravi71@yahoo.com>
2024-02-05 16:57:55 +0000

    data

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ import pandas as pd
 from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
+import shutil
 
 
 refrence = osm
-zoom = 8
+zoom = 11
 s = datetime.utcnow()
 for region in [f.stem for f in Path('data/Tile numbers/'+str(zoom)).iterdir()]:
     numbs = pd.read_csv('/workspaces/Tile-Server/data/Tile numbers/'+str(zoom)+'/'+region+'.csv')
     with ThreadPoolExecutor(max_workers=100) as pool:
         pool.map(refrence.downloader,numbs['X'], numbs['Y'], [zoom]*len(numbs))
 e = datetime.utcnow()
+shutil.make_archive('/workspaces/Tile-Server/output/osm/'+str(zoom), 'zip', '/workspaces/Tile-Server/output/osm', str(zoom))
 print(e-s)
 
 
